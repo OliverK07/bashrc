86c24b342314b0cf34983fb051fad4ad087774c8
oliverke <oliver.ke@prismtech.com>
2017-09-26 10:47:05 +0800

    remove vim settings ;)

--- install.sh
+++ install.sh
@@ -17,16 +17,18 @@ function soft_link() {
 
 log_info "backing up current vim config..."
 today=`date +%Y%m%d`
-related_files=( $HOME/.vimrc $HOME/.bashrc $HOME/.bash_profile $HOME/.gitconfig )
+#related_files=( $HOME/.vimrc $HOME/.bashrc $HOME/.bash_profile $HOME/.gitconfig)
+related_files=( $HOME/.bashrc $HOME/.bash_profile $HOME/.gitconfig)
 for i in "${related_files[@]}"; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today ; done
 for i in "${related_files[@]}"; do [ -L $i ] && unlink $i ; done
 
 log_info "setting up symlinks..."
-soft_link $CURRENT_DIR/vimrc $HOME/.vimrc
+#soft_link $CURRENT_DIR/vimrc $HOME/.vimrc
 #soft_link $CURRENT_DIR/vim $HOME/.vim
 soft_link $CURRENT_DIR/bashrc $HOME/.bashrc
 soft_link $CURRENT_DIR/bash_profile $HOME/.bash_profile
 soft_link $CURRENT_DIR/gitconfig $HOME/.gitconfig
+#soft_link $CURRENT_DIR/vim $HOME/.vim
 
 log_info "source bashrc..."
 source ~/.bashrc
